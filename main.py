ad97cbaf56fac7ef9a296c244b0a116d1d5ed7c1
Frederic GONTIER <fredgont@yahoo.fr>
2024-07-10 16:46:34 +0200

    ajout de traces

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from datetime import datetime
 if __name__ == '__main__':
     items = []
     for dept in ['22', '44', '35', '49', '50', '56', '53', '72']:
-        items.extend(immobilier_notaires_webservices.download_annonces_du_jour_departement(dept))
+        new_annonces = immobilier_notaires_webservices.download_annonces_du_jour_departement(dept)
+        print("%s annonces" % len(new_annonces))
+        items.extend(new_annonces)
 
     with open('annonces/immobilier_notaires_%s.json' % (datetime.now().strftime('%Y%m%d')), 'w') as fp:
         json.dump(items, fp)
